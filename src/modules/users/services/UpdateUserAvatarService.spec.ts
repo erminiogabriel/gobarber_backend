71abc3b6d3b3d57a2087d4b4c3d0f6f3a8ea1c50
Gabriel Machado <gabriel@movitech.com.br>
2021-01-19 17:45:50 -0300

    Desenvolvimento dos testes para o servico de SendForgotPasswordEmail

--- src/modules/users/services/UpdateUserAvatarService.spec.ts
+++ src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -43,4 +43,35 @@ describe('UpdateUsersAvatar', () => {
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
+
+  it('should delete old avatar when updating new one', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeStorageProvider = new FakeStorageProvider();
+
+    const deleteFile = jest.spyOn(fakeStorageProvider, 'deleteFile');
+
+    const updateUserAvatar = new UpdateUserAvatarService(
+      fakeUsersRepository,
+      fakeStorageProvider,
+    );
+
+    const user = await fakeUsersRepository.create({
+      name: 'gabriel',
+      email: 'gabriel@movitech.com.br',
+      password: 'senha123',
+    });
+
+    await updateUserAvatar.execute({
+      avatarFilename: 'avatar.jpg',
+      user_id: user.id,
+    });
+
+    await updateUserAvatar.execute({
+      avatarFilename: 'avatar2.jpg',
+      user_id: user.id,
+    });
+
+    expect(deleteFile).toHaveBeenCalledWith('avatar.jpg');
+    expect(user.avatar).toBe('avatar2.jpg');
+  });
 });
