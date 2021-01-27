bfbd49cba18cd586ee8ebe542aad7f2b38eb321f
Gabriel Machado <gabriel@movitech.com.br>
2021-01-27 11:52:03 -0300

    Adicionado o template de email utilizando o handlebars.

---
+++ src/modules/users/services/UpdateProfileService.spec.ts
@@ -0,0 +1,102 @@
+// import AppError from '@shared/errors/AppError';
+import FakeHashProvider from '@modules/users/providers/HashProvider/fakes/FakeHashProvider';
+import AppError from '@shared/errors/AppError';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import UpdateProfileService from './UpdateProfileService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let fakeHashProvider: FakeHashProvider;
+let updateProfileService: UpdateProfileService;
+
+describe('UpdateProfile', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeHashProvider = new FakeHashProvider();
+    updateProfileService = new UpdateProfileService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+  });
+  it('should be able to update the profile', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'teste',
+      email: 'teste@gobarber.com.br',
+      password: '123456',
+    });
+    const updatedUser = await updateProfileService.execute({
+      user_id: user.id,
+      name: 'nome_atualizado',
+      email: 'novoemail@gobarber.com.br',
+    });
+    expect(updatedUser.name).toBe('nome_atualizado');
+    expect(updatedUser.email).toBe('novoemail@gobarber.com.br');
+  });
+  it('should not be able to change to another user email', async () => {
+    await fakeUsersRepository.create({
+      name: 'gabriel',
+      email: 'gabriel@gobarber.com.br',
+      password: '123456',
+    });
+    const user = await fakeUsersRepository.create({
+      name: 'teste',
+      email: 'teste@gobarber.com.br',
+      password: '123456',
+    });
+
+    await expect(
+      updateProfileService.execute({
+        user_id: user.id,
+        name: 'gabriel',
+        email: 'gabriel@gobarber.com.br',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+  it('should be able to update the password', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'teste',
+      email: 'teste@gobarber.com.br',
+      password: '123456',
+    });
+    const updatedUser = await updateProfileService.execute({
+      user_id: user.id,
+      name: 'gabriel',
+      email: 'gabriel@gobarber.com.br',
+      old_password: '123456',
+      password: 'abcdef',
+    });
+    expect(updatedUser.name).toBe('gabriel');
+    expect(updatedUser.email).toBe('gabriel@gobarber.com.br');
+    expect(updatedUser.password).toBe('abcdef');
+  });
+  it('should not be able to update the password without old password', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'teste',
+      email: 'teste@gobarber.com.br',
+      password: '123456',
+    });
+    await expect(
+      updateProfileService.execute({
+        user_id: user.id,
+        name: 'gabriel',
+        email: 'gabriel@gobarber.com.br',
+        password: 'abcdef',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+  it('should not be able to update the password with wrong old password', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'teste',
+      email: 'teste@gobarber.com.br',
+      password: '123456',
+    });
+    await expect(
+      updateProfileService.execute({
+        user_id: user.id,
+        name: 'gabriel',
+        email: 'gabriel@gobarber.com.br',
+        password: 'abcdef',
+        old_password: 'wrong_old_password',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
--- src/modules/users/services/UpdateUserAvatarService.spec.ts
+++ src/modules/users/services/UpdateUserAvatarService.spec.ts
@@ -3,16 +3,21 @@ import FakeStorageProvider from '@shared/container/providers/StorageProvider/fak
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import UpdateUserAvatarService from './UpdateUserAvatarService';
 
-describe('UpdateUsersAvatar', () => {
-  it('should be able to update avatar', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeStorageProvider = new FakeStorageProvider();
+let fakeUsersRepository: FakeUsersRepository;
+let fakeStorageProvider: FakeStorageProvider;
+let updateUserAvatar: UpdateUserAvatarService;
 
-    const updateUserAvatar = new UpdateUserAvatarService(
+describe('UpdateUsersAvatar', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeStorageProvider = new FakeStorageProvider();
+
+    updateUserAvatar = new UpdateUserAvatarService(
       fakeUsersRepository,
       fakeStorageProvider,
     );
-
+  });
+  it('should be able to update avatar', async () => {
     const user = await fakeUsersRepository.create({
       name: 'gabriel',
       email: 'gabriel@movitech.com.br',
@@ -28,14 +33,6 @@ describe('UpdateUsersAvatar', () => {
   });
 
   it('should not be able to update avatar from non existing user', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeStorageProvider = new FakeStorageProvider();
-
-    const updateUserAvatar = new UpdateUserAvatarService(
-      fakeUsersRepository,
-      fakeStorageProvider,
-    );
-
     await expect(
       updateUserAvatar.execute({
         avatarFilename: 'avatar.jpg',
@@ -45,15 +42,7 @@ describe('UpdateUsersAvatar', () => {
   });
 
   it('should delete old avatar when updating new one', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeStorageProvider = new FakeStorageProvider();
-
     const deleteFile = jest.spyOn(fakeStorageProvider, 'deleteFile');
-
-    const updateUserAvatar = new UpdateUserAvatarService(
-      fakeUsersRepository,
-      fakeStorageProvider,
-    );
 
     const user = await fakeUsersRepository.create({
       name: 'gabriel',
